ff7e0dc80b3d40275d0b88f9324f28287ffa8c7d
Michael Sotir <sotirm@umich.edu>
2020-04-29 20:45:21 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(an_array, a_string)
+  an_array.push(a_string)
+end
+
+def using_unshift(an_array, a_string)
+  an_array.unshift a_string
+end
+
+def using_pop(an_array)
+  an_array.pop()
+end
+
+def pop_with_args
+  
+end
